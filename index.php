779c30fdf8e8f2a175d2aac4fb0b71decd51aa2b
Vicinities Application <vicinities1@gmail.com>
2017-02-03 11:25:49 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Pawprint</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/pawprint-styles.css">
+	<link href="https://fonts.googleapis.com/css?family=Architects+Daughter" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Mitr:200" rel="stylesheet">
+	<script src="/js/bootstrap.min.js"></script>
+</head>
+<div align="center" class="navigation-buttons hidden-xs hidden-sm">
+	<button class="btn btn-default navigation"><a href="#">Browse</a></button>
+	<button class="btn btn-default navigation"><a href="#">About Us</a></button>
+	<button class="btn btn-default navigation"><a href="#"><img class="header-logo" src="img/logo.png"></a></button>
+	<button class="btn btn-default navigation"><a href="#">Sign Up</a></button>
+	<button class="btn btn-default navigation"><a href="#">Login</a></button>
+</div>
+
+<div align="center" class="navigation-buttons hidden-md hidden-lg">
+	<div align="center"><button class="btn btn-default navigation"><a href="#">Browse</a></button></div>
+	<div align="center"><button class="btn btn-default navigation"><a href="#">About Us</a></button></div>
+	<div align="center"><button class="btn btn-default navigation"><a href="#"><img class="header-logo" src="img/logo.png"></a></button></div>
+	<div align="center"><button class="btn btn-default navigation"><a href="#">Sign Up</a></button></div>
+	<div align="center"><button class="btn btn-default navigation"><a href="#">Login</a></button></div>
+</div>
+
+<body>
+	<p class="welcome">help out a shelter. <span class="foster">foster</span> a rescue with <span class="pawprint">Pawprint</span></p>
+	<div align="center">
+		<button class="btn btn-default browse" type="submit">Browse Now</button>
+	</div>
+
+</body>
+	<div class="footer" align="center">
+		<img class="footer-dog" src="img/dog.jpg">
+	</div>
+</html>
